81e446500c571886cda5c951aedc5e2741241e1f
ML\ob86 <adnan.abbasi@sunlife.com>
2023-12-06 16:20:32 -0500

    Implemented Villa Model

---
+++ WhiteLagoon.Infrastructure/Migrations/20231206210414_updatingVillasColumns.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatingVillasColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Update_Date",
+                table: "Villas",
+                newName: "Updated_Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Updated_Date",
+                table: "Villas",
+                newName: "Update_Date");
+        }
+    }
+}
