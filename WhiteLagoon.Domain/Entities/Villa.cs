ca0795c65bef539301d5036db28b5881a7aa2c67
ML\ob86 <adnan.abbasi@sunlife.com>
2023-12-06 13:47:13 -0500

    Implemented Project Setup

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,15 @@
+﻿namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Update_Date { get; set; }
+    }
+}
